c0a7f85c75feac902451769e5ad2201a3dde35be
ASUS <devikakp2013@gmail.com>
2023-10-07 10:19:23 +0530

    webdriver

--- src/aaction/Copyandpaste__keyboard.java
+++ src/aaction/Copyandpaste__keyboard.java
@@ -1,14 +1,13 @@
 package aaction;
 
-import java.awt.Desktop.Action;
-import java.awt.event.ActionEvent;
+//import java.awt.Desktop.Action;
+//import java.awt.event.ActionEvent;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.interactions.Actions;//import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
--- src/datadrivennn/Datadrivenspreadsheet.java
+++ src/datadrivennn/Datadrivenspreadsheet.java
@@ -1,7 +1,7 @@
 package datadrivennn;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
